5d98644cdcd9a223f7f129ef9770774bc5783cec
Lucassamuel97 <lukassamuka88@gmail.com>
2025-04-02 23:26:19 -0300

    :sparkles: feat: Refatora ListCategory e adiciona componente CategoryTable para exibição de categorias

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, debounce, IconButton, Typography } from "@mui/material"
-import { useSnackbar } from "notistack";
-import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import { deleteCategory, selectCategories, useDeleteCategoryMutation, useGetCategoriesQuery } from "./categorySlice";
-import { Link } from "react-router-dom";
-import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp, GridToolbar } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp, GridToolbar } from '@mui/x-data-grid';
+import { useSnackbar } from "notistack";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../app/hooks";
+import { useDeleteCategoryMutation, useGetCategoriesQuery } from "./categorySlice";
 
 
 export const CategoryList = () => {
---
+++ src/features/categories/components/CategoryTable.tsx
@@ -0,0 +1,36 @@
+import { IconButton, Typography } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridFilterModel,
+  GridRenderCellParams,
+  GridToolbar,
+} from "@mui/x-data-grid";
+import { Results } from "../../../types/Category";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/system";
+
+type Props = {
+  data: Results | undefined;
+  perPage: number;
+  isFetching: boolean;
+  rowsPerPage?: number[];
+
+  handleOnPageChange: (page: number) => void;
+  handleFilterChange: (filterModel: GridFilterModel) => void;
+  handleOnPageSizeChange: (perPage: number) => void;
+  handleDelete: (id: string) => void;
+};
+
+export function CategoriesTable({
+  data,
+  perPage,
+  isFetching,
+  rowsPerPage,
+  handleOnPageChange,
+  handleFilterChange,
+  handleOnPageSizeChange,
+  handleDelete,
+}: Props) {
+}
