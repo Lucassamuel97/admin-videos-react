56d242d08bfcf5318569b7a5218530085d5bd7d5
Lucassamuel97 <lukassamuka88@gmail.com>
2025-04-19 16:15:05 -0300

    :test_tube: test: Adiciona testes para o componente EditCategory com simulação de requisições

---
+++ src/features/categories/EditCategory.test.tsx
@@ -0,0 +1,74 @@
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
+vi.mock("react-router-dom", async () => {
+    const actual = await vi.importActual("react-router-dom");
+    return {
+      ...actual,
+      useParams: () => ({ id: "1" }), // o ID que seu componente espera
+    };
+});
+
+import { renderWithProviders, screen, waitFor, fireEvent } from "../../utils/test-utils";
+import { CategoryEdit } from "./EditCategory";
+import { baseUrl } from "../api/apiSlice";
+
+const data = {
+    id: "1",
+    name: "Category 1",
+    description: "Category test",
+    is_active: true,
+    deleted_at: null,
+    created_at: "2022-09-27T17:10:33+0000",
+    updated_at: "2022-09-27T17:10:33+0000",
+};
+
+export const handlers = [
+    http.get(`${baseUrl}/categories/:id`, async ({ params }) => {
+        return HttpResponse.json({data});
+    }),
+    http.put(`${baseUrl}/categories/${data.id}`, async () => {
+        await new Promise(resolve => setTimeout(resolve, 200));
+        return HttpResponse.json(
+            {data},
+            { status: 201 }
+        );
+    }
+    ),
+];
+
+const server = setupServer(...handlers);
+
+describe("EditCategory", () => {
+    afterAll(() => server.close());
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+
+    it("should render correctly", () => {
+        const { asFragment } = renderWithProviders(<CategoryEdit />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("should handle submit", async () => {
+        renderWithProviders(<CategoryEdit />);
+        const name = await screen.findByTestId("name");
+        const description = screen.getByTestId("description");
+        const isActive = screen.getByTestId("is_active");
+        const submit = screen.getByText("Save");
+
+        await waitFor(() => {
+            expect(name).toHaveValue("Category 1");
+        });
+
+        fireEvent.change(name, { target: { value: "test" } });
+        fireEvent.change(description, { target: { value: "test desc" } });
+        fireEvent.click(isActive);
+        fireEvent.click(submit);
+
+        await waitFor(() => {
+            const text = screen.getByText("Category updated successfully");
+            expect(text).toBeInTheDocument();
+        });
+    
+    });
+});
