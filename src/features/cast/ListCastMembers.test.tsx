0bbcd1effe19924c072096a4128fe933fb4d2100
Lucassamuel97 <lukassamuka88@gmail.com>
2025-04-19 18:32:41 -0300

    :test_tube: test: Adiciona testes para o componente ListCastMembers, incluindo manipulação de paginação, busca e tratamento de casos sem registros

--- src/features/cast/ListCastMembers.test.tsx
+++ src/features/cast/ListCastMembers.test.tsx
@@ -1,17 +1,24 @@
 import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
-import { renderWithProviders, screen, waitFor } from "../../utils/test-utils";
+import { fireEvent, renderWithProviders, screen, waitFor } from "../../utils/test-utils";
 import { baseUrl } from "../api/apiSlice";
-import { castMemberResponse } from "../mocks/castMemberMock";
-import { categoryResponsePage2 } from "../mocks/categoryMock";
+import { castMemberResponse, castMemberResponseNoRows, castMemberResponsePage2 } from "../mocks/castMemberMock";
 import { CastMemberList } from "./ListCastMembers";
 
 export const handlers = [
     http.get(`${baseUrl}/cast_members`, async (request) => {
-        const page = new URL(request.request.url).searchParams.get("page");
+        const url = new URL(request.request.url);
+        const page = url.searchParams.get("page");
+        const search = url.searchParams.get("search")
+         
         if (page === "2") {
-            return HttpResponse.json(categoryResponsePage2);
+            return HttpResponse.json(castMemberResponsePage2);
         }
+
+        if (search == "semregistros") {
+            return HttpResponse.json(castMemberResponseNoRows);
+        }
+
         await new Promise(resolve => setTimeout(resolve, 200));
         return HttpResponse.json(castMemberResponse);
     }),
@@ -22,7 +29,7 @@ const server = setupServer(...handlers);
 describe("ListCastmembers", () => {
     beforeAll(() => server.listen());
     afterEach(() => server.resetHandlers());
-    afterAll(() => server.close());
+    afterAll(() => server.close());''
 
     it("should render correctly", () => {
         const { asFragment } = renderWithProviders(<CastMemberList />);
@@ -60,4 +67,56 @@ describe("ListCastmembers", () => {
             expect(error).toBeInTheDocument();
         });
     });
+
+    it("should handle On PageChange", async () => {
+        renderWithProviders(<CastMemberList />);
+    
+        await waitFor(() => {
+          const name = screen.getByText("Teste");
+          expect(name).toBeInTheDocument();
+        });
+    
+        const nextButton = screen.getByTestId("KeyboardArrowRightIcon");
+        fireEvent.click(nextButton);
+    
+        await waitFor(() => {
+          const name = screen.getByText("Teste 2");
+          expect(name).toBeInTheDocument();
+        });
+      });
+
+      it("should handle filter change", async () => {
+        renderWithProviders(<CastMemberList />);
+    
+        await waitFor(() => {
+          const name = screen.getByText("Teste");
+          expect(name).toBeInTheDocument();
+        });
+    
+        const input = screen.getByPlaceholderText("Search…");
+        fireEvent.change(input, { target: { value: "Teste" } });
+    
+        await waitFor(() => {
+          const loading = screen.getByRole("progressbar");
+          expect(loading).toBeInTheDocument();
+        });
+      });
+
+        it("should handle no rows", async () => {
+            renderWithProviders(<CastMemberList />);
+        
+            await waitFor(() => {
+            const name = screen.getByText("Teste");
+            expect(name).toBeInTheDocument();
+            });
+        
+            const input = screen.getByPlaceholderText("Search…");
+            fireEvent.change(input, { target: { value: "semregistros" } });
+        
+            await waitFor(() => {
+            const noRows = screen.getByText("No rows");
+            expect(noRows).toBeInTheDocument();
+            });
+        });
+    
 });
