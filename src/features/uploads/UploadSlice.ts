137d38a4d8c892599f36a0e35075ba9fd82d6399
Lucassamuel97 <lukassamuka88@gmail.com>
2025-05-28 22:47:35 -0300

    sparkles: feat:  adiciona a remoção de uploads ao Uploadslice

--- src/features/uploads/UploadSlice.ts
+++ src/features/uploads/UploadSlice.ts
@@ -1,37 +1,44 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export const STATUS = {
-  IDDLE: "idle",
-  LOADING: "loading",
-  SUCCESS: "success",
-  FAILED: "failed",
+    IDDLE: "idle",
+    LOADING: "loading",
+    SUCCESS: "success",
+    FAILED: "failed",
 } as const;
 
 export type Status = typeof STATUS[keyof typeof STATUS];
 
 export interface UploadState {
-  id: string;
-  file: File;
-  field: string;
-  videoId: string;
-  progress?: number;
-  status?: Status;
+    id: string;
+    file: File;
+    field: string;
+    videoId: string;
+    progress?: number;
+    status?: Status;
 }
 
 const initialState: UploadState[] = [];
 
 const uploadsSlice = createSlice({
-  name: "uploads",
-  initialState,
-  reducers: {
-    addUpload(state, action: PayloadAction<UploadState>) {
-      state.push({ ...action.payload, status: "idle", progress: 0 });
+    name: "uploads",
+    initialState,
+    reducers: {
+        addUpload(state, action: PayloadAction<UploadState>) {
+            state.push({ ...action.payload, status: "idle", progress: 0 });
+        },
+        removeUpload(state, action: PayloadAction<string>) {
+            const index = state.findIndex((upload) => upload.id === action.payload);
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
     },
-  },
 });
 
 export const {
-  addUpload
+    addUpload,
+    removeUpload,
 } = uploadsSlice.actions;
 
 export const uploadReducer = uploadsSlice.reducer;
